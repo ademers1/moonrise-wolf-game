5260ea76d6bc3b009b78b022c9a63d0de9153d08
ademers1 <ademers145@live.ca>
2019-10-24 15:11:51 -0400

    Updating Unity Version #
    Using 2019.2.6

---
+++ CapstoneGame/Assets/Scripts/AutoSaveOnRun.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+[InitializeOnLoad]
+public class AutosaveOnRun: ScriptableObject
+{
+	static AutosaveOnRun()
+	{
+		EditorApplication.playmodeStateChanged += () =>
+		{
+			if(EditorApplication.isPlayingOrWillChangePlaymode && !EditorApplication.isPlaying)
+			{
+				Debug.Log("Auto-Saving scene before entering Play mode: " + EditorSceneManager.GetActiveScene());
+
+                EditorSceneManager.SaveOpenScenes();
+				AssetDatabase.SaveAssets();
+			}
+		};
+	}
+}
