1e62b7e875d166c6853c2abf7269b8f9d9fa22d5
adrianndoe <53951589+adrianndoe@users.noreply.github.com>
2019-10-30 12:29:28 -0400

    Created FP Camera
    Updated the script First Person Camera now operational. Movement still needed.

--- CapstoneGame/Assets/Scripts/FirstPersonCamera.cs
+++ CapstoneGame/Assets/Scripts/FirstPersonCamera.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class FirstPersonCamera : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float mouseSens = 100f;
+    public Transform playerBody;
+    private float xRotation = 0f;
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
-        
+        float mouseX = Input.GetAxis("Mouse X") * mouseSens * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSens * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+       
     }
 }
