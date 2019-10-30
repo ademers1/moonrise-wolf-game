222ceb6b58817e75988641592130eb822ce05979
TooFairytale <53872823+TooFairytale@users.noreply.github.com>
2019-10-30 12:06:00 -0400

    Started Inventory
    Getting new changes

---
+++ CapstoneGame/Assets/Scripts/InventorySystem.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventorySystem : MonoBehaviour
+{
+    public GameObject Items;
+    public List<GameObject> Inventory = new List<GameObject>();
+
+    private void Start()
+    {
+       
+
+    }
+}
