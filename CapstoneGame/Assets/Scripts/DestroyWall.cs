058306f767da4a2e2ea14977554d5e4a626a7fd2
adrianndoe <53951589+adrianndoe@users.noreply.github.com>
2019-10-30 11:17:49 -0400

    Fixed
    Fixed master copy

---
+++ CapstoneGame/Assets/Scripts/DestroyWall.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyWall : MonoBehaviour
+{
+    public GameObject wall4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Created by ~ The Leprechaun");
+    }
+
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Destroy(wall4, 0.1f);
+        }
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Meow");
+        }
+    }
+}
---
+++ CapstoneGame/Assets/Scripts/NextScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class NextScene : MonoBehaviour
+{
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
+}
