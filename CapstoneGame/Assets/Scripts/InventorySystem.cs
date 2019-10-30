1b611e75d518a6c38a4a82337291bf8f350d0a80
TooFairytale <53872823+TooFairytale@users.noreply.github.com>
2019-10-30 12:31:22 -0400

    Update InventorySystem.cs
    Pick up added

--- CapstoneGame/Assets/Scripts/InventorySystem.cs
+++ CapstoneGame/Assets/Scripts/InventorySystem.cs
@@ -7,9 +7,31 @@ public class InventorySystem : MonoBehaviour
     public GameObject Items;
     public List<GameObject> Inventory = new List<GameObject>();
 
-    private void Start()
+    //Item functions on pick up
+    private void OnCollisionEnter(Collision collision)
     {
-       
+        Items = collision.gameObject;
+        if(Items.tag == "item1")
+        {
+            Inventory.Insert(0,Items) ;
+            Debug.Log("item1 picked up");
+        }
+        if (Items.tag == "item2")
+        {
+            Inventory.Insert(1,Items);
+            Debug.Log("item2 picked up");
+        }
+        if (Items.tag == "item3")
+        {
+            Inventory.Insert(2,Items);
+            Debug.Log("item3 picked up");
+        }
+    }
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
 
+        }
     }
 }
