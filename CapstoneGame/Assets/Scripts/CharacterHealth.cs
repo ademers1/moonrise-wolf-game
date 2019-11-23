3d1b995601c9ba0db2ef7e423b85a95a7d917a7f
adrianndoe <53951589+adrianndoe@users.noreply.github.com>
2019-11-23 14:53:34 -0500

    HealthSystem
    Working on quick easy UI for health bar, combat system also in the works.

---
+++ CapstoneGame/Assets/Scripts/CharacterHealth.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CharacterHealth : MonoBehaviour
+{
+    private float health;
+    public float startHealth;
+    public Image healthBar;
+
+    void Start()
+    {
+        health = startHealth;
+    }
+
+    void Update()
+    {
+     if(health <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+        healthBar.fillAmount -= health/startHealth;
+    }
+    public void Die()
+    {
+        Destroy(gameObject);
+        SceneManager.LoadScene("Game");
+    }
+}
