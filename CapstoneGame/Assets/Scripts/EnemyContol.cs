f5186a632b7774aeee29999cf69fdfbd3605b412
adrianndoe <53951589+adrianndoe@users.noreply.github.com>
2019-11-23 16:16:11 -0500

    UI/EnemyCombat
    Updated enemy lockon Target. Shooting coming up next.

--- CapstoneGame/Assets/Scripts/EnemyContol.cs
+++ CapstoneGame/Assets/Scripts/EnemyContol.cs
@@ -4,15 +4,73 @@ using UnityEngine;
 
 public class EnemyContol : MonoBehaviour
 {
-    // Start is called before the first frame update
+   public Transform target;
+    public float range = 15f;
+    public string enemyTag = "Player";
+    private float speed = 10;
+
+    public float fireRate = 1f;
+    private float fireCountDown = 0f;
+
+
     void Start()
     {
-        
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
     }
 
-    // Update is called once per frame
+    void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if(distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if (nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+        }
+        else
+        {
+            target = null;
+        }
+    }
     void Update()
     {
-        
+        if (target == null)
+        {
+            return;
+        }
+
+        Vector3 dir = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+        Vector3 rotation = Quaternion.Lerp(transform.rotation, lookRotation, Time.deltaTime * speed).eulerAngles;
+        transform.rotation = Quaternion.Euler(0f,rotation.y,0f);
+
+        if(fireCountDown <=0)
+        {
+            Shoot();
+            fireCountDown = 1f / fireRate;
+        }
+
+        fireCountDown -= Time.deltaTime;
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range); 
+    }
+
+    void Shoot()
+    {
+        Debug.Log("Shoot");
     }
 }
