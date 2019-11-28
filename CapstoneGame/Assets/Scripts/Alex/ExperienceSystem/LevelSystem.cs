3bf8bf4b469fa297620fdf83bd6f8b12931f524c
ademers1 <ademers145@live.ca>
2019-11-27 22:20:16 -0500

    Started level system
    Updated UI so that it's all in one palce and added in level system class as a beginning for level ups

---
+++ CapstoneGame/Assets/Scripts/Alex/ExperienceSystem/LevelSystem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSystem 
+{
+    private int level;
+    private int currentExperience;
+    private int totalExperience;
+    private int experienceToNextLevel;
+
+    public LevelSystem()
+    {
+        level = 0;
+        currentExperience = 0;
+        experienceToNextLevel = 100;
+    }
+
+    public void AddExperience(int amount)
+    {
+        currentExperience += amount;
+        totalExperience += amount;
+        if(currentExperience >= experienceToNextLevel)
+        {
+            level++;
+            currentExperience -= experienceToNextLevel;
+        }
+    }
+}
