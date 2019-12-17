54738b54c10233cbaf70ef867bbffde00d3df75d
TooFairytale <53872823+TooFairytale@users.noreply.github.com>
2019-12-17 15:42:53 -0500

    melee patrol enemy

---
+++ CapstoneGame/Assets/Scripts/Daniel/MeleeEnemy/MeleeEnemy.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.AI;
+
+
+public class MeleeEnemy : MonoBehaviour
+{
+    Rigidbody rb;
+    public GameObject player;
+    public GameObject enemy;
+    public Transform playerTransform;
+    public bool isRunning;
+    public Animator anim;
+    public MeshRenderer rend;
+    NavMeshAgent agent;
+    public GameObject key;
+    public ParticleSystem particle;
+    public Transform[] nodeObj;
+    bool isPatrol;
+    int randomSpot;
+    bool alerted;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+        rend = GetComponent<MeshRenderer>();
+        agent = GetComponent<NavMeshAgent>();
+        randomSpot = Random.Range(0, nodeObj.Length);
+        isPatrol = true;
+        GoOnPatrol();
+    }
+
+    void Update()
+    {
+
+        if(Vector3.Distance(playerTransform.position, transform.position) < 10)
+        {
+            transform.LookAt(playerTransform);
+            transform.localScale = new Vector3(1, 1, 1);
+            isRunning = true;
+            alerted = true;
+            agent.SetDestination(playerTransform.position);
+        }
+        
+        if(!alerted)
+        {
+            isPatrol = true;
+            GoOnPatrol();
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag=="Player")
+        {
+            
+            Destroy(collision.gameObject);
+            SceneManager.LoadScene(1);
+        }
+    }
+    public void EnemyDie()
+    {
+        Destroy(this.gameObject);
+    }
+    public void dropItem(GameObject I)
+    {
+        Instantiate(I, transform.position, Quaternion.identity);
+    }
+    public void dieParticleActive()
+    {
+        Instantiate(particle, transform.position, Quaternion.identity);
+    }
+    void GoOnPatrol()
+    {
+        if (isPatrol == true)
+        {
+            agent.destination = nodeObj[randomSpot].position;
+            if (Vector3.Distance(enemy.transform.position, nodeObj[randomSpot].position) < 1f)
+            {
+                randomSpot = Random.Range(0, nodeObj.Length);
+            }
+        }
+    }
+}
