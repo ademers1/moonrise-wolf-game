84253c5a584cc831096390e1e45a1fb34ed1624a
TooFairytale <53872823+TooFairytale@users.noreply.github.com>
2020-05-26 16:07:31 -0400

    WolfSenese

---
+++ CapstoneGame/Assets/Scripts/Daniel/WolfSense.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WolfSense : MonoBehaviour
+{
+    [SerializeField] private float senseRadius;
+    [SerializeField] private float senseStopRadius;
+    [SerializeField] private float senseTimer;
+    bool wolfSenseOn;
+    public LayerMask enemyLayers;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        senseRadius = 30.0f;
+        senseStopRadius = 200.0f;
+        senseTimer = 1.0f;
+        wolfSenseOn = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (wolfSenseOn == false)
+            {
+                WolfSensing();
+            }
+            else if (wolfSenseOn == true)
+            {
+                WolfSensingStop();
+            }
+        }
+        if(wolfSenseOn == true)
+        {
+            Collider[] colliders = Physics.OverlapSphere(transform.position, senseRadius, enemyLayers);
+            foreach (Collider enemy in colliders)
+            {
+                if (enemy.GetComponent<WolfSenseMaterial>() != null)
+                {
+                    enemy.GetComponent<WolfSenseMaterial>().beingSensed = true;
+                }
+                if (Vector3.Distance(enemy.transform.position, transform.position) > senseRadius && enemy.GetComponent<WolfSenseMaterial>() != null)
+                {
+                    enemy.GetComponent<WolfSenseMaterial>().beingSensed = false;
+                }
+
+            }
+        }
+        
+
+    }
+    void WolfSensing()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, senseRadius, enemyLayers);
+        foreach (Collider enemy in colliders)
+        {
+            if(enemy.GetComponent<WolfSenseMaterial>() != null)
+            {
+                enemy.GetComponent<WolfSenseMaterial>().beingSensed = true;
+            }
+            
+        }
+        wolfSenseOn = true;
+    }
+   void WolfSensingStop()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, senseStopRadius, enemyLayers);
+        foreach (Collider enemy in colliders)
+        {
+            if (enemy.GetComponent<WolfSenseMaterial>() != null)
+            {
+                enemy.GetComponent<WolfSenseMaterial>().beingSensed = false;
+
+            }
+        }
+        wolfSenseOn = false;
+    }
+    
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, senseRadius);
+    }
+}
