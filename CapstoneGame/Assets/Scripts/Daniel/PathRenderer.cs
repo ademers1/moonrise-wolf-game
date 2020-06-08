2ec402e181eb39fbe729582b5e16bdc5a43ceb17
TooFairytale <53872823+TooFairytale@users.noreply.github.com>
2020-06-07 23:19:03 -0400

    Made path from player to a enemy visible

---
+++ CapstoneGame/Assets/Scripts/Daniel/PathRenderer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PathRenderer : MonoBehaviour
+{
+    public LineRenderer line; //to hold the line Renderer
+    [SerializeField]
+    Transform targetTransform; //to hold the transform of the target
+    public NavMeshAgent agent; //to hold the agent of this gameObject
+
+    void Awake()
+    {
+        line = GetComponent<LineRenderer>(); //get the line renderer
+        agent = GameObject.Find("Player").GetComponent<NavMeshAgent>(); //get the agent
+    }
+
+    private void Update()
+    {
+        getPath();
+    }
+    void getPath()
+    {
+        line.SetPosition(0, agent.transform.position); //set the line's origin
+
+        agent.SetDestination(targetTransform.position); //create the path
+
+        DrawPath(agent.path);
+
+        agent.isStopped = true;//add this if you don't want to move the agent
+
+    }
+
+    void DrawPath(NavMeshPath path)
+    {
+        if (path.corners.Length < 2) //if the path has 1 or no corners, there is no need
+            return;
+
+        line.positionCount = path.corners.Length; //set the array of positions to the amount of corners
+
+        for (var i = 1; i < path.corners.Length; i++)
+        {
+            line.SetPosition(i, path.corners[i]); //go through each corner and set that to the line renderer's position
+        }
+    }
+}
