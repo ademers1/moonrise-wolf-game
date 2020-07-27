cb2441a64b02aa97c8bf42dec4218734cab74d2a
N Gardiner <neil_gardiner@live.ca>
2020-07-27 12:41:26 -0400

    Added basic Enemy health system
    Basic Enemy health system that will need to call to Player Attack script for damage amounts. Still needs to be flushed out but I am having technical issues with system. Will be completeed by 6pm Tuesday july 28th latest.

---
+++ CapstoneGame/Assets/Scripts/NPC/NPCTakeDamage.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCTakeDamage : MonoBehaviour
+{
+
+    public int damageToGive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Enemy")
+        {
+            //Destroy(other.gameObject);
+            other.gameObject.GetComponent<NPCHealth>().HurtEnemy(damageToGive);
+        }
+    }
+}
