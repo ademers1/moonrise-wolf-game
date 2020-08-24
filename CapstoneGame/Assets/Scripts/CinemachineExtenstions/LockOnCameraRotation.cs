17ebb9334e7b12e6b46de5b5dc56774672853002
TooFairytale <53872823+TooFairytale@users.noreply.github.com>
2020-08-24 18:22:24 -0400

    Fixing Lock on

---
+++ CapstoneGame/Assets/Scripts/CinemachineExtenstions/LockOnCameraRotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using Cinemachine.Utility;
+
+public class LockOnCameraRotation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
