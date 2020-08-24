199ed01d618d10468140f7bc747a7a50fbb46087
adrianndoe <53951589+adrianndoe@users.noreply.github.com>
2020-08-24 16:32:22 -0400

    PuzzleCreation
    Implemented puzzle. Basic and needs improvement.

---
+++ CapstoneGame/Assets/Scripts/GateScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateScript : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Chase"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
