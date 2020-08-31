2529099a40530e6297addf8ea095f296b9f800df
adrianndoe <53951589+adrianndoe@users.noreply.github.com>
2020-08-31 01:38:35 -0400

    FollowNPC
    added movment and changes to chase scene.

--- CapstoneGame/Assets/ChaseMovement.cs
+++ CapstoneGame/Assets/ChaseMovement.cs
@@ -1,41 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.AI;
 public class ChaseMovement : MonoBehaviour
 {
     public float speed = 10f;
     public float lookRadius = 10f;
-    int current = 0;
+    public NavMeshAgent agent;
     GameObject target;
+    Rigidbody rb;
+    public float runTimer = 2f;
     public Transform[] runLocations;
-    float WPradius = 1f;
+    private int pointIndex;
     // Start is called before the first frame update
     void Start()
     {
         target = GameObject.Find("Player");
-       
+        rb = GetComponent<Rigidbody>();
+        agent = GetComponent<NavMeshAgent>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
         float distance = Vector3.Distance(target.transform.position, transform.position);
-        if(distance <= lookRadius)
+        if (distance <= lookRadius)
         {
-           if(Vector3.Distance(runLocations[current].position, transform.position) < WPradius)
-            {
-                current++;
-                if(current >= runLocations.Length)
-                {
-                    current = 0;
-                }
-            }
-            transform.position = Vector3.MoveTowards(transform.position, runLocations[current].position, speed * Time.deltaTime);
+            RunAway();
         }
- 
-    
     }
 
     private void OnDrawGizmosSelected()
@@ -44,11 +37,32 @@ public class ChaseMovement : MonoBehaviour
         Gizmos.DrawWireSphere(transform.position, lookRadius);
     }
 
+    void RunAway()
+    {
+            if (runLocations.Length == 0) return;
+            if (Vector3.Distance(this.transform.position, runLocations[pointIndex].position) < 0.3f)
+            {
+                pointIndex++;
+                if (pointIndex >= runLocations.Length)
+                {
+                    pointIndex = 0;
+                }
+            }
+        agent.SetDestination(runLocations[pointIndex].position);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.CompareTag("Gate"))
+        if (other.gameObject.CompareTag("Gate"))
         {
             Destroy(gameObject);
         }
+    }
+
+    IEnumerator IndexChange()
+    {
+        yield return new WaitForSeconds(runTimer);
+        pointIndex++;
+        agent.destination = runLocations[pointIndex].position;
     }
 }
